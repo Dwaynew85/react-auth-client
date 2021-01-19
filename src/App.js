ac70e2984abf2fb1b89467290cdbe0c5ed3c0657
Dwaynew85 <mr_dwayne_w@yahoo.com>
2021-01-19 13:45:32 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,64 @@
+import React, { Component } from 'react';
 import './App.css';
+import Login from "./components/Login"
 
-function App() {
-  return (
-    <div className="App">
-      Auth Lecture June 03 2019
-    </div>
-  );
+export class App extends Component {
+  constructor() {
+    super()
+  
+    this.state = {
+       currentUser: null,
+       loginForm: {
+         email: "", 
+         password: ""
+       }
+    }
+  }
+
+  handleLoginFormChange = event => {
+    const { name, value } = event.target
+    this.setState({
+      loginForm: {
+        ...this.state.loginForm,
+        [name]: value
+      }
+
+    })
+  }
+
+  handleLoginFormSubmit = event => {
+    event.preventDefault()
+    const userInfo = this.state.LoginForm
+    const headers = {
+      method: 'POST',
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        user: userInfo
+      })
+    }
+    fetch("http://localhost:3001/login", headers)
+    .then(response => response.json())
+    .then(console.log)
+    .catch(console.log)
+  }
+  
+
+
+  render() {
+    return (
+      <div className="App">
+        Welcome User
+        <Login
+          handleLoginFormChange={this.handleLoginFormChange}
+          handleLoginFormSubmit={this.componentDidCatchhandleLoginFormSubmit}  
+          email={this.state.loginForm.email}
+          password={this.state.loginForm.password}
+        />
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
